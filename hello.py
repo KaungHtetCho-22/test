46f4762525aaf7d8d45d7f593fbc6a80e9b9c9ea
Kaung Htet Cho <141211281+KaungHtetCho-22@users.noreply.github.com>
2024-10-14 15:58:57 +0000

    add initial layout of python project

---
+++ hello.py
@@ -0,0 +1,14 @@
+def more_hello():
+    return "hi"
+
+
+def more_goodbye():
+    return "goodbye"
+
+
+# this is nonsense code that generates warning
+# var = 1
+# var = var
+
+# # bad syntax
+# foo =
---
+++ test_hello.py
@@ -0,0 +1,13 @@
+from hello import more_hello, more_goodbye
+
+
+def test_more_hello():
+    assert "hi" == more_hello()
+
+
+# def test_more_hello2():
+#     assert 'hello' == more_hello()
+
+
+def test_more_goodbye():
+    assert "goodbye" == more_goodbye()
